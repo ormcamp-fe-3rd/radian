4e5ff696952d6e2f2028d22c73397f02614f2358
incolore9 <hansol.woff@gmail.com>
2024-12-07 03:55:06 +0900

    [추가] 체크박스 유효성 필수, 선택, 전체선택 기능

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,54 +3,76 @@ import '../styles/reset.css';
 import '../styles/Register.css';
 
 const Register = (): JSX.Element => {
+  const [isChecked, setIsChecked] = useState([false, false, false]);
   const [signUp, setSignUp] = useState([
     {
       title: '라디안 서비스 이용 약관 동의 (필수)',
       content: '서비스 본문 (추후 추가)',
-      status: false,
+      buttonStatus: false,
     },
     {
       title: '라디안 개인정보 수집 이용 동의 (필수)',
       content: '개인정보 본분 (추후 추가)',
-      status: false,
+      buttonStatus: false,
     },
     {
       title: '마케팅 정보 수신에 동의합니다. (선택)',
       content: '마케팅 동의 (추후 추가)',
-      status: false,
+      buttonStatus: false,
     },
   ]);
   return (
     <>
       <ul className="container">
         <li className="list-item">
-          <input type="checkbox" className="circle-check"></input>
+          <input
+            type="checkbox"
+            className="circle-check"
+            checked={isChecked[0] && isChecked[1] && isChecked[2]}
+            onClick={() => {
+              const copy = [...isChecked];
+              copy[0] = !copy[0];
+              copy[1] = !copy[1];
+              copy[2] = !copy[2];
+              setIsChecked(copy);
+            }}
+          ></input>
           아래 내용을 모두 확인했으며 전체 동의합니다.
         </li>
         {signUp.map((e, i) => (
           <li>
             <div
-              className={`list-item ${e.status === true ? 'no-border' : ''}`}
+              className={`list-item ${e.buttonStatus === true ? 'no-border' : ''}`}
             >
-              <input type="checkbox" className="circle-check" />
+              <input
+                type="checkbox"
+                className="circle-check"
+                checked={isChecked[i]}
+                onClick={() => {
+                  let copy = [...isChecked];
+                  copy[i] = !copy[i];
+                  setIsChecked(copy);
+                }}
+              />
               {e.title}
               <button
                 className="read-more"
                 onClick={() => {
                   let copy = [...signUp];
-                  copy[i].status = !copy[i].status;
+                  copy[i].buttonStatus = !copy[i].buttonStatus;
                   setSignUp(copy);
                 }}
               >
                 더보기
               </button>
             </div>
-            {e.status === true ? (
+            {e.buttonStatus === true ? (
               <div className="content-box">{e.content}</div>
             ) : null}
           </li>
         ))}
       </ul>
+      {isChecked[0] && isChecked[1] === true ? alert('체크 완료') : null}
     </>
   );
 };
