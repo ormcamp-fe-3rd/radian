b44fdc784b571dcd204e59f049f80b3ae83f253b
ksj0621 <r06t21@gmail.com>
2024-12-08 06:50:07 +0900

    [초안]로그인페이지(로그인페이지 컴포넌트 초안 작성)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // react-query 개발 툴 사용 시
+
+/**page */
+import Login from '/src/pages/Login';
 
 const queryClient = new QueryClient(); //추가
 
 const App = (): JSX.Element => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <h1>Hello, Radian!</h1>
-      </div>
-      {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <h1>Hello, Radian!</h1>
+          <Routes>
+            <Route path="/login" element={Login} />
+          </Routes>
+        </div>
+
+        {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
+    </Router>
   );
 };
 
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,3 +1,40 @@
+import './styles/Login.css';
 
+const LoginForm: React.FC = () => {
+    return (
+        <>
+            <section className="login-form">
+                <form className="input-container" action="">
+                    <div className="int-area">
+                        <input
+                            type="text"
+                            name="id"
+                            id="name-input"
+                            required />
+                        <label
+                            id="id"
+                            htmlFor='name-input'>ID/E-MAIL</label>
+                    </div>
 
+                    <div className="int-area">
+                        <input
+                            type="password"
+                            name="password"
+                            id="password-input"
+                            required />
+                        <label
+                            id="pw"
+                            htmlFor='password-input'>PASSWORD</label>
+                    </div>
+                </form>
 
+                <button id="btn-login" type="submit">LOGIN</button>
+                <div id="register">
+                    <a rel='register' href="" target="_blank">REGISTER</a>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default LoginForm;
