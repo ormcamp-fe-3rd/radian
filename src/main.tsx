314c52c0a721842c81242b0911d2bcdc889a89f1
incolore9 <hansol.woff@gmail.com>
2024-12-07 15:22:36 +0900

    [추가] 회원가입 페이지 라우터 등록 /register

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import './styles/App.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
