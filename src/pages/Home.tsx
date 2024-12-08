f8f92d8ae5c04354a6cb1dc1e5bac9c053e17c19
incolore9 <hansol.woff@gmail.com>
2024-12-09 00:33:55 +0900

    [추가] css 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,11 +24,10 @@ const Home = (): JSX.Element => {
     window.addEventListener('scroll', () => {
       const greenBox = greenBoxRef.current.getBoundingClientRect();
       const blackBox = blackSectionRef.current.getBoundingClientRect();
-      if (greenBox.top === 0) {
-        let scrolledDistance = Math.min(
-          1,
-          Math.abs(blackBox.top / window.innerHeight).toFixed(1),
-        );
+      if (greenBox.top == 0) {
+        let scrolledDistance = Math.abs(
+          blackBox.top / window.innerHeight,
+        ).toFixed(1);
         setDivWidth(scrolledDistance);
         console.log(divWidth);
       } else if (greenBox.top >= 0) {
@@ -41,16 +40,29 @@ const Home = (): JSX.Element => {
     <>
       <div className="styles.container">
         <div className="video-container">
+          <a tabIndex={0}>Classic but Electronic</a>
           <video className="index-video" autoPlay muted loop>
             <source src="/videos/sample-cars-loop-clip.mp4" type="video/mp4" />
           </video>
         </div>
+        <h1 className="home-title">
+          We have a bold vision for the future of travel where you can better
+          connect with the outdoors and each other. Lightship’s electric travel
+          trailers allow you to visit new corners of the world while doing your
+          part to preserve it.
+        </h1>
         <div className="black-section" ref={blackSectionRef}>
           <div
             ref={greenBoxRef}
             className="green-box"
             style={{ width: `${divWidth * 100}vw` }}
-          ></div>
+          >
+            <img
+              className="image-fit"
+              src="https://monochrome-watches.com/wp-content/uploads/2020/10/Mini-Cooper-Classic-Electric-conversion-1.jpg"
+              alt="Mini Cooper Classic Electric"
+            />
+          </div>
         </div>
         <div className="red-box" ref={aboutCompanyTitle}></div>
       </div>
