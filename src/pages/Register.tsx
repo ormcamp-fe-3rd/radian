6c00c6b716951f44cc13f66f86cf34a93a783f47
incolore9 <hansol.woff@gmail.com>
2024-12-09 16:25:48 +0900

    [추가] 유효성검사 라이브러리 적용, 기본 정규식 및 에러메세지 추가

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../styles/reset.css';
 import '../styles/Register.css';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-// import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 const Register = (): JSX.Element => {
   return (
@@ -105,31 +105,91 @@ const Agreement = (): JSX.Element => {
 };
 
 const SignInForm = (): JSX.Element => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm();
+
+  const password = watch('password');
+
   return (
     <>
       <div className="container">
-        <form action="">
+        <form
+          noValidate
+          onSubmit={handleSubmit((data) => alert('가입이 완료되었습니다.'))}
+        >
+          <label htmlFor="name"></label>
           <input
-            placeholder="성함을 입력해주세요."
+            id="name"
+            placeholder="이름을 입력해주세요."
             className="input-form"
             type="text"
+            {...register('name', {
+              value: 5,
+              required: '이름은 필수 입력값입니다.',
+            })}
+            aria-invalid={errors.name ? 'true' : 'false'}
           />
+          {errors.name && <small>{errors.name.message}</small>}
+
+          <label htmlFor="email"></label>
           <input
+            id="email"
             placeholder="이메일 형식으로 아이디를 입력해주세요"
             className="input-form"
             type="email"
+            {...register('email', {
+              required: '이메일은 필수 입력값입니다.',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: '이메일 형식에 맞지 않습니다.',
+              },
+            })}
+            aria-invalid={errors.email ? 'true' : 'false'}
           />
+          {errors.email && <small>{errors.email.message}</small>}
+          <label htmlFor="password"></label>
           <input
-            placeholder="비밀번호는 숫자, 특수문자, 대소문자를 포함하여 8자 이상 입력해주세요"
+            id="password"
+            placeholder="비밀번호는 8자 이상 입력해주세요"
             className="input-form"
             type="password"
+            {...register('password', {
+              required: '비밀번호는 필수 입력값입니다.',
+              pattern: {
+                value: /^.{8,}$/,
+                message: '비밀번호는 8자리 이상 입력해주세요.',
+              },
+            })}
+            aria-invalid={errors.password ? 'true' : 'false'}
           />
+          {errors.password && <small>{errors.password.message}</small>}
+          <label htmlFor="passwordConfirm"></label>
           <input
+            id="passwordConfirm"
             placeholder="상단의 비밀번호를 한번 더 입력해주세요"
             className="input-form"
             type="password"
+            {...register('passwordConfirm', {
+              required: '비밀번호 확인은 필수입니다.',
+              validate: (value) =>
+                value === password || '비밀번호가 일치하지 않습니다.',
+            })}
+            aria-invalid={errors.passwordConfirm ? 'true' : 'false'}
           />
-          <button className="next-step-button">가입하기</button>
+          {errors.passwordConfirm && (
+            <small>{errors.passwordConfirm.message}</small>
+          )}
+          <button
+            className="next-step-button"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            가입하기
+          </button>
         </form>
       </div>
     </>
