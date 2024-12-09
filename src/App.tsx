f5059a1b90ea033aaabaac9d775479e1ddfb7277
structified <kimwooju@gmail.com>
2024-12-09 15:58:01 +0900

    [초안]초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import './App.css';
+import './styles/App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // react-query 개발 툴 사용 시
+import ProductReservation from './pages/ProductReservation';
+import { Route, Routes } from 'react-router-dom';
 
 const queryClient = new QueryClient(); //추가
 
 const App = (): JSX.Element => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <h1>Hello, Radian!</h1>
-      </div>
-      {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
+      <Routes>
+        <Route path="/productreservation/*" element={<ProductReservation />} />
+      </Routes>      {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
