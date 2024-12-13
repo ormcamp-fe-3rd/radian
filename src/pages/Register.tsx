9146d26f7e9802ad454e04c9a8baa432c8d473b6
incolore9 <hansol.woff@gmail.com>
2024-12-13 16:14:34 +0900

    [수정] 헤더, 푸터 두개씩 출력되는 것 수정

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,13 +7,11 @@ import SignInForm from '../components/SignInForm';
 const Register = (): JSX.Element => {
   return (
     <>
-      <HeaderLogin />
       <h2 className="register-title">회원 가입</h2>
       <Routes>
         <Route path="/" element={<Agreement />} />
         <Route path="/form" element={<SignInForm />} />
       </Routes>
-      <Footer />
     </>
   );
 };
