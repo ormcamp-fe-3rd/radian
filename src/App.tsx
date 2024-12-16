6662a120206f2bff37291a5f8bc92551c036b452
incolore9 <hansol.woff@gmail.com>
2024-12-16 18:16:11 +0900

    [수정] 라우터 중복 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './styles/reset.css';
 import './styles/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // react-query 개발 툴 사용 시
 
@@ -20,26 +20,26 @@ const queryClient = new QueryClient(); //추가
 
 const App = (): JSX.Element => {
   return (
-    <Router>
-      <QueryClientProvider client={queryClient}>
-        
-        <Header />
-        
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/register/*" element={<Register />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/product-list" element={<ProductList />}></Route>
-          <Route path="/product-detail" element={<ProductDetail />}></Route>
-          <Route path="/product-reservation/*" element={<ProductReservation />}></Route>
-        </Routes>
-        
-        <Footer />
+    <QueryClientProvider client={queryClient}>
+      <Header />
 
-        {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register/*" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/product-list" element={<ProductList />}></Route>
+        <Route path="/product-detail" element={<ProductDetail />}></Route>
+        <Route
+          path="/product-reservation/*"
+          element={<ProductReservation />}
+        ></Route>
+      </Routes>
+
+      <Footer />
+
+      {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 };
 
