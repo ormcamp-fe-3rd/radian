f39aa63a6bacaf8d3344698c30bd24ddb426fb24
parkHansol <hansol.woff@gmail.com>
2024-12-16 18:24:40 +0900

    [수정] 라우터 중복 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './styles/reset.css';
 import './styles/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // react-query 개발 툴 사용 시
 
@@ -20,7 +20,6 @@ const queryClient = new QueryClient(); //추가
 
 const App = (): JSX.Element => {
   return (
-    <Router>
       <QueryClientProvider client={queryClient}>
         
         <Header />
@@ -39,7 +38,6 @@ const App = (): JSX.Element => {
         {/* react-query 개발 툴 사용 시 가장 하위에 위치하게 작성 */}
         <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
-    </Router>
   );
 };
 
