e7327c31f01ad5ae769d688849a20beacfb4e548
incolore9 <hansol.woff@gmail.com>
2024-12-24 01:01:56 +0900

    [수정] 타이틀 상수 선언 및 파일 분리

---
+++ src/data/companyVision.tsx
@@ -0,0 +1,11 @@
+const COMPANY_VISION = {
+  mainTitle: `We have a bold vision for the future of travel, presenting ways for
+          you to better connect with nature and each other. Radian’s electric
+          classic cars allow you to explore new corners of the world while
+          providing the joy of preserving the environment along the way.`,
+
+  subTitle: `We breathe new life into the timeless beauty of classic car design
+          with modern electric vehicle technology.`,
+};
+
+export default COMPANY_VISION;
