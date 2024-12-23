254ba26f83998ea65dde4f94902e15fd643c7ead
ksj0621 <r06t21@gmail.com>
2024-12-23 15:57:23 +0900

    [수정]경로 수정

--- src/components/ProductDetail/ScrollHeader.tsx
+++ src/components/ProductDetail/ScrollHeader.tsx
@@ -9,7 +9,7 @@ interface ScrollHeaderProps {
 const ScrollHeader = ({ carData }: ScrollHeaderProps) => {
   return (
     <div className="detail-header">
-      <Link to={`product-list`}>
+      <Link to={`/product-list`}>
         <svg className="detail-go-list" version="1.1" viewBox="0 0 24 24">
           <rect x="3" y="3" width="7" height="7"></rect>
           <rect x="14" y="3" width="7" height="7"></rect>
