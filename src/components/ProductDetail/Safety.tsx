3310fbcc662f1eeebe3dd6299b672c670178ae67
ksj0621 <r06t21@gmail.com>
2024-12-27 11:27:01 +0900

    [수정]상세페이지(세이프티 스크롤 삭제)

--- src/components/ProductDetail/Safety.tsx
+++ src/components/ProductDetail/Safety.tsx
@@ -16,32 +16,8 @@ const Safety = ({ carData }: SafetyProps) => {
         <div className="horizontal-scroll-wrapper">
           <div className="img-wrapper slower">
             <img src={carData.safetyImages1} alt="safety1" />
-          </div>    
-          <div className="img-wrapper faster">
-            <img src={carData.safetyImages2} alt="safety2" />
-          </div>    
-          <div className="img-wrapper slower vertical">
-            <img src={carData.safetyImages3} alt="safety3" />
-          </div>    
-          <div className="img-wrapper slower slower-down">
-            <img src={carData.safetyImages4} alt="safety4" />
-          </div>    
-          <div className="img-wrapper slower faster1">
-            <img src={carData.safetyImages5} alt="safety5" />
-          </div>    
-          <div className="img-wrapper slower slower2">
-            <img src={carData.safetyImages6} alt="safety6" />
-          </div>    
-          <div className="img-wrapper slower last">
-            <img src={carData.safetyImages7} alt="safety7" />
-          </div>    
-          <div className="img-wrapper slower">
-            <img src={carData.safetyImages8} alt="safety8" />
-          </div>    
-          <div className="img-wrapper slower">
-            <img src={carData.safetyImages9} alt="safety9" />
-          </div>    
-        </div>
+          </div>
+        </div>   
 
         <Link to={`/product-reservation/${carData.id}`}>
           <button className="safety-build-btn">Build Now</button>
